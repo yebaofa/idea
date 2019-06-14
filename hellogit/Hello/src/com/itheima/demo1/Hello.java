61fce58fccbb310ecaa19f92e11e3b09d7963966
yebaofa <229441341@qq.com>
2019-06-14 08:43:55 +0800

    Hello2

--- hellogit/Hello/src/com/itheima/demo1/Hello.java
+++ hellogit/Hello/src/com/itheima/demo1/Hello.java
@@ -4,4 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("helloworld");
     }
+    public void show(){
+        System.out.println("你好");
+    }
 }
